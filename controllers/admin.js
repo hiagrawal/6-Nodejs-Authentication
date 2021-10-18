b72abe8388eb8ef7ae7dadc0344f3dc41a68d27a
hiagrawal <agrawal.hina13@gmail.com>
2021-10-18 18:35:28 +0530

    Adding CRSF token to all views POST requests through locals defined in app js file

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,8 +7,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false,
-    isAuthenticated: req.session.isLoggedIn
+    editing: false
   });
 };
 
