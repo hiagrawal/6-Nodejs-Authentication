8b5342dbc483f77c5c3d8f081bcc131e46269cea
hiagrawal <agrawal.hina13@gmail.com>
2021-10-20 19:20:59 +0530

    Redirecting to the password reset form if token matches and expiration date is greater than current date and time

--- controllers/auth.js
+++ controllers/auth.js
@@ -157,7 +157,8 @@ exports.postReset = (req, res, next) => {
         req.flash('error', 'No account with that email found');
         return res.redirect('/reset');
       }
-      user.resetToken = token;
+      //user.resetToken = token;
+      user.resetToken = '12345';
       user.resetTokenExpiration = Date.now() + 3600000;
       return user.save();
     })
@@ -180,4 +181,25 @@ exports.postReset = (req, res, next) => {
     })
     .catch(err => console.log(err));
   })
-}
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+  .then(user => {
+    let message = req.flash('error');
+    if(message.length > 0){
+      message = message[0];
+    } else {
+      message = null;
+    }
+    res.render('auth/new-password', {
+      path: '/new-password',
+      pageTitle: 'New Password',
+      errorMessage: message,
+      userId: user._id.toString()
+    });
+  })
+  .catch(err => console.log(err));
+  
+};
