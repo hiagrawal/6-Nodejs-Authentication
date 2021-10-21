58aeb1e8a191755fd687ee7237a7d1f4e4492292
hiagrawal <agrawal.hina13@gmail.com>
2021-10-21 14:54:16 +0530

    Validation for confirm password

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,15 @@ router.post(
 
     body('password', 'Please enter a password with only numbers and text and atleast 5 characters.')
     .isLength({min:5})
-    .isAlphanumeric()
+    .isAlphanumeric(),
+
+    body('confirmPassword')
+    .custom((value, {req}) => {
+        if(value !== req.body.password){
+            throw new Error('Passwords have to match!');
+        }
+        return true;
+    })
 ],
 
 authController.postSignup
