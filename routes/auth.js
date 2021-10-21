b8160b2926fa7130a0e4aeb2236f5653551f5de5
hiagrawal <agrawal.hina13@gmail.com>
2021-10-21 13:35:27 +0530

    Using Custom Validations

--- routes/auth.js
+++ routes/auth.js
@@ -12,10 +12,24 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email') ,authController.postSignup);
 //will add a 'validation middleware' on signup, so it will check for form input controls and if error will add it in req object 
 //that we can access in postSignUp controller method uisng 'validationResult' method provided by 'express-validator' pkg
 //the names inside validation middleware check method is the form control name on signup.ejs page/view
+//can use inbuilt validators or can give custom validations using 'custom' method which takes a method
+router.post(
+'/signup', 
+check('email')
+.isEmail()
+.withMessage('Please enter a valid email')
+.custom((value, {req}) => {
+    if(value === 'test@test.com'){
+        throw new Error('This email address is forbidden');
+    }
+    return true;
+}),
+authController.postSignup
+);
+
 
 router.post('/logout', authController.postLogout);
 
