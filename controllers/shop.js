f3936f4e9bf8f851388b385cf2ebee0f92aaa276
hiagrawal <agrawal.hina13@gmail.com>
2021-10-25 21:05:17 +0530

    Generating pdf files with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -172,7 +172,19 @@ exports.getInvoice = (req, res, next) => {
     pdfDoc.pipe(res); //as well as serve it to the server
 
     //Now will start writing to file
-    pdfDoc.text('Hello World!');
+    //pdfDoc.text('Hello World!');
+
+    pdfDoc.fontSize(26).text('Invoice', {
+      underline: true
+    })
+    pdfDoc.text('---------------------------');
+    let totalPrice = 0;
+    order.products.forEach(prod => {
+      totalPrice += prod.quantity * prod.product.price;
+      pdfDoc.fontSize(14).text(prod.product.title + '-' + prod.quantity + 'x' + '$' + prod.product.price);
+    });
+    pdfDoc.text('---------------------------');
+    pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
     pdfDoc.end(); //end will indicate that it is end od document and it has finished writing
 
