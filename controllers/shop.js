c3231e4e47b416d35ed1b9aae799d7749de6595f
hiagrawal <agrawal.hina13@gmail.com>
2021-10-26 14:13:35 +0530

    Reusing the same pagination logic in product list page and so making common pagination.ejs page

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,18 +9,42 @@ const Order = require('../models/order');
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  // Product.find()
+  //   .then(products => {
+  //     console.log(products);
+  //     res.render('shop/product-list', {
+  //       prods: products,
+  //       pageTitle: 'All Products',
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().count().then(numProducts => {
+      totalItems = numProducts;
+      return Product.find().skip((page-1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
-      console.log(err);
-    });
+      next(new Error(err));
+    }); 
+
 };
 
 exports.getProduct = (req, res, next) => {
