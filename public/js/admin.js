b08e63225c55d88538ac53324f7377054e1d968b
hiagrawal <agrawal.hina13@gmail.com>
2021-10-26 16:34:37 +0530

    Updating the dom to remove the delete product from dom as well

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,9 +1,11 @@
 const deleteProduct = (btn) => {
-    console.log(btn);
+   console.log(btn);
    const prodId = btn.parentNode.querySelector('[name=productId]').value;
    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
    console.log(prodId);
    console.log(csrf);
+
+   const productElement = btn.closest('article');
 
    fetch('/admin/product/' + prodId, {
     method: 'DELETE',
@@ -13,6 +15,16 @@ const deleteProduct = (btn) => {
    })
    .then(result => {
        console.log(result);
+       return result.json();
+   })
+   .then(data => {
+       console.log(data);
+       //works on all modern browsers (not on IE)
+       //productElement.remove();
+
+       //works on all browsers including IE
+       productElement.parentNode.removeChild(productElement);
+       
    })
    .catch(err => {
        console.log(err);
