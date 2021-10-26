973afed20399f0d62b6cbcbace715988f6f44303
hiagrawal <agrawal.hina13@gmail.com>
2021-10-26 15:54:59 +0530

    Adding client side javascript code for delete functionality and removing form post submit on it

---
+++ public/js/admin.js
@@ -0,0 +1,7 @@
+const deleteProduct = (btn) => {
+    console.log(btn);
+   const prodId = btn.parentNode.querySelector('[name=productId]').value;
+   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+   console.log(prodId);
+   console.log(csrf);
+};
